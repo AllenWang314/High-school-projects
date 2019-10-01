036588f68a094fd26eff632c84f0252f1be19816
allenwang <allenwang@18.20.216.215>
2019-10-01 16:02:33 -0400

    First Commit

---
+++ USACO Practice/src/HRTemplate.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class HRTemplate {
+    public static void main(String args[] ) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> info = new ArrayList<Integer>();
+        while (scanner.hasNext()){
+            info.add(Integer.parseInt(scanner.next()));
+        }
+    }
+}
+
