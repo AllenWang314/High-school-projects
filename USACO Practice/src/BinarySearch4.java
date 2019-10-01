8be70a79a801b5c162c62aa308d2296e9dbfb2ad
allenwang <allenwang@dhcp-18-20-179-195.dyn.mit.edu>
2019-10-01 16:15:48 -0400

    First Commit

---
+++ USACO Practice/src/BinarySearch4.java
@@ -0,0 +1,21 @@
+
+public class BinarySearch4 {
+	
+	// inclusive of both low and high
+	public static int bs(int low, int high) {
+		while(high > low + 1) {
+			int mid = (low + high)/2;
+			if(possible(mid)) high = mid;
+			else low = mid;
+		}
+		return high;
+	}
+	
+	public static boolean possible(int test) {
+		return (test > 30);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(bs(1, 900));
+	}
+}
