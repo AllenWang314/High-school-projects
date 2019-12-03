db582c28d9b3f400e5c3ebf1e23cb50a1d6a451c
allenwang <allenwang@192.168.223.103>
2019-12-03 16:00:30 -0500

    cleared code

--- USACO Solution/src/Solution.java
+++ USACO Solution/src/Solution.java
@@ -1,21 +1,5 @@
-import java.io.*;
 
-
-public class Solution {
-
-	public static void makeTest() throws IOException {
-		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("file.in")));
-		pw.write("4\n" + 
-				"1 2 4 3");
-		pw.close();
-	}
+class Solution {
 	
-	@SuppressWarnings({ "unused", "resource" })
-	public static void main(String[] args) throws IOException {
-		makeTest();
-		BufferedReader br = new BufferedReader(new FileReader("file.in"));
-		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("file.out")));
-		Solution s = new Solution();
-		
-	}
+	
 }
